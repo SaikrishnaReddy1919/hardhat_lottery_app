3d41247c2f97cd7825bdad4956590057eb0ec533
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2022-11-10 19:42:59 +0530

    tests for checkUpKeep

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -35,7 +35,6 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
               it("records players when they enter", async function () {
                   await raffle.enterRaffle({ value: raffleEntranceFee })
                   const player = await raffle.getPlayer(0)
-                  console.log(player, deployer)
                   assert.equal(player, deployer)
               })
 
@@ -60,6 +59,44 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.be.revertedWith(
                       "Raffle__NotOpen"
                   )
+              })
+          })
+          describe("checkUpKeep", async function () {
+              it("returns false if people dont sent any ETH", async function () {
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  await network.provider.send("evm_mine", [])
+                  /**
+                   * Rather than executing the state-change of a transaction, it is possible to ask a node to pretend that a call is not state-changing and return the result.
+                   * This does not actually change any state, but is free. This in some cases can be used to determine if a transaction will fail or succeed.
+                   */
+                  const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([])
+                  assert(!upkeepNeeded)
+              })
+              it("returns false if raffle isn't open", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  await network.provider.send("evm_mine", [])
+
+                  await raffle.performUpkeep([])
+                  const raffleState = await raffle.getRaffleState()
+                  const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([])
+                  assert.equal(raffleState.toString(), "1")
+                  assert.equal(upkeepNeeded, false)
+              })
+
+              it("returns false if enough time hasn't passed", async () => {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() - 5]) // use a higher number here if this test fails
+                  await network.provider.request({ method: "evm_mine", params: [] })
+                  const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x") // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+                  assert(!upkeepNeeded)
+              })
+              it("returns true if enough time has passed, has players, eth, and is open", async () => {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  await network.provider.request({ method: "evm_mine", params: [] })
+                  const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x") // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+                  assert(upkeepNeeded)
               })
           })
       })
